d36f5d116c59fdfb5152e2ee8c9aafc3f079553b
dyh <dongyh1995@126.com>
2018-11-19 09:57:46 -0500

    real time

--- pose_detection.py
+++ pose_detection.py
@@ -16,11 +16,11 @@ def initial():
 	if CUDA:
 		model = model.cuda()
 
-	cktp_file = './checkpoint/ckpt_160.pth.tar'
-	# cktp_file = './checkpoint/gt_ckpt_best.pth.tar'
+	# cktp_file = './checkpoint/ckpt_160.pth.tar'
+	cktp_file = './checkpoint/gt_ckpt_best.pth.tar'
 	ckpt = torch.load(cktp_file, map_location=lambda storage, loc: storage)
-	model.load_state_dict(ckpt['model'])
-	# model.load_state_dict(ckpt['state_dict'])
+	# model.load_state_dict(ckpt['model'])
+	model.load_state_dict(ckpt['state_dict'])
 	return CUDA, model
 
 def pose_detection(input, model, CUDA):
